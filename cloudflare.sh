7bc52d41c9e1e0648b319954181f0c787ab9ea6a
HackTFTP <97189175+HackTFTP@users.noreply.github.com>
2024-07-06 23:38:37 -0400

    Create cloudflare.sh

---
+++ cloudflare.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiNmNlYTcyZWMyMzVjODNhOTBmODVlNTY0YjE3MDA2MjUiLCJ0IjoiYjIyMDlhOTItZjAzZC00OTVmLTgwZDAtZjViMGM4OTcxM2E5IiwicyI6Ik1HUmtZV0ZpTjJZdE5XWmhZeTAwTjJNMUxUaG1PREF0WlRCbU5XTTVaRFk1TVRJMCJ9
